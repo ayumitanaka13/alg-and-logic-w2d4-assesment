111f1d269e143eae83e814151fcb409b0d061f38
ayumitanaka <tanakayumi13@gmail.com>
2020-09-24 11:52:04 -0700

    add answers

--- halfSorted.js
+++ halfSorted.js
@@ -11,8 +11,22 @@ let arr2 = [56, 98, 65, 3, 4, 58, 68, 90, 12, 34, 45]
 
 const halfSorted = function(arr) {
 
-};
+    let mid = Math.floor(arr.length / 2);
+    let arr1 = [];
+    let arr2 = [];
 
+    for (let i = 0; i < mid - 1; i++) {
+        arr1.push(arr[i]);
+        arr1.sort((a, b) => a - b);        
+    }
+    for (let j = mid; j < arr.length; j++) {
+        arr2.push(arr[j])
+        arr2.sort((a, b) => b - a)
+    }
+
+    let newArr = arr1.concat(arr2);
+    return newArr;
+};
 
 console.log(halfSorted(arr1));
 
--- insertionSort.js
+++ insertionSort.js
@@ -8,9 +8,24 @@ let arr2 = [56, 98, 65, 3, 4, 58, 68, 90, 12, 34, 45];
 
 
 const insertionSort = function(arr) {
+    
+    for (let i = 0; i < arr.length; i++) {
+        
+        let current = arr[i];
+        let j = i - 1;
 
+        while ((j > -1) && (current < arr[j])) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+
+    return arr;
 };
 
 console.log(insertionSort(arr1));
+// [ 2,  4,  6,  7,  8, 23, 34, 45, 54, 77, 78, 99, 567 ]
 
 console.log(insertionSort(arr2));
+// [ 3,  4, 12, 34, 45, 56, 58, 65, 68, 90, 98 ]
